696a63ea0decfdb949c28110c18c27e124423886
gooler <1090240688@qq.com>
2016-04-25 19:44:52 +0800

    apple

---
+++ js/apple.js
@@ -0,0 +1,60 @@
+$(function(){
+    var $minNavL=$('.min-navL');
+    var $maxNav=$('.max-nav');
+    var flag=true;
+    var $banner=$('.banner');
+    var $all=$('.all');
+    var $h3=$('.footer h3');
+    var $ul=$('.footer ul');
+    var flag1=true;
+    var w=document.documentElement.clientWidth;
+    $minNavL.click(function(){
+        if(flag){
+            $maxNav.css({display:'block'})
+            $banner.css({display:'none'})
+            $all.hide();
+            flag=false;
+        }else if(!flag){
+            $maxNav.css({display:'none'})
+            $banner.css({display:'block'})
+            $all.show();
+            flag=true;
+        }
+
+    })
+    //window.onresize=function(){
+    //    w=document.documentElement.clientWidth;
+    //}
+        $h3.each(function(i,obj){
+            console.log(w)
+
+            $(this).click(function(){
+                if( document.documentElement.clientWidth<=760){
+                if(flag1){
+                    $ul.eq(i).show();
+                    flag1=false;
+                }else {
+                    $ul.eq(i).hide();
+                    flag1=true;
+                }
+                }else if(w>760){
+                    return false;
+                }
+            })
+
+        })
+
+
+    //alert(w)
+    //if(w<760){
+    //    $(this).click(function(){
+    //        if(flag1){
+    //            $ul.eq(i).show();
+    //            flag1=false;
+    //        }else {
+    //            $ul.eq(i).hide();
+    //            flag1=true;
+    //        }
+    //})
+
+})
